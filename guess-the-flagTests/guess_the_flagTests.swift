2f37fa045dd41506bf8afc8755d7df47a13add01
Jeremy Tandjung <jeremytandjung@icloud.com>
2020-06-19 15:00:42 -0700

    first commit

---
+++ guess-the-flagTests/guess_the_flagTests.swift
@@ -0,0 +1,34 @@
+//
+//  guess_the_flagTests.swift
+//  guess-the-flagTests
+//
+//  Created by Jeremy's Macbook Pro 16 on 6/19/20.
+//  Copyright © 2020 Jeremy Tandjung. All rights reserved.
+//
+
+import XCTest
+@testable import guess_the_flag
+
+class guess_the_flagTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
